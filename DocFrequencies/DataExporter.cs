08491881f3ce3b70f406d4367a921c935d01938e
Movsar Bekaev <bekav.movsar@gmail.com>
2017-09-29 22:13:38 +0300

    style works

--- DocFrequencies/DataExporter.cs
+++ DocFrequencies/DataExporter.cs
@@ -3,6 +3,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 using SpreadsheetLight;
 using SpreadsheetLight.Charts;
 using System;
+using System.Windows.Forms;
 
 namespace wFrequencies
 {
@@ -11,14 +12,30 @@ namespace wFrequencies
         public static void Export(ObjectListView olv, string filePath)
         {
             SLDocument sl = new SLDocument();
-            SLStyle slstyle = new SLStyle();
-            slstyle.Fill.PatternBackgroundColor = Color.Red;
+            SLStyle style = sl.CreateStyle();
+
+            //   sl.SetCellStyle(6, 5, style);
 
 
-            sl.SetCellValue("B3", "It costs what for a Jimmy Choo?!?");
-            sl.SetCellStyle("B3", slstyle);
 
-            // POOF! Chart done.
+            for (int i = 1; i <= olv.Columns.Count; ++i) {
+                sl.SetCellValue(1, i, olv.Columns[i - 1].Text);
+            }
+
+
+            for (int i = 1; i <= olv.Columns.Count; ++i) {
+                for (int j = 1; j <= olv.Items.Count; ++j) {
+                    string cellVal = olv.Items[j - 1].SubItems[i - 1].Text;
+                    System.Drawing.Color backColor = olv.Items[j - 1].BackColor;
+                    System.Drawing.Color foreColor = olv.Items[j - 1].ForeColor;
+
+
+                    style.Fill.SetPattern(PatternValues.Solid, backColor, foreColor);
+
+                    sl.SetCellValue(j + 1, i, cellVal);
+                    sl.SetCellStyle(j + 1, i, style);
+                }
+            }
 
             sl.SaveAs(filePath);
         }
